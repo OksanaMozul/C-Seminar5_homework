a49abbed6e08442ecfbecc24567da4db1e2433bf
Oksana <oksana-k83@mail.ru>
2023-03-17 16:16:03 +0300

    Решенв задача 1 семинара 5

--- Task_1-3.cs
+++ Task_1-3.cs
@@ -1,8 +1,42 @@
 ﻿// Урок 5. Функции и одномерные массивы
+
 // Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
+Console.WriteLine("Задайте длину массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+RandonNumbers(numbers);
+Console.WriteLine("В массиве: ");
+PrintArray(numbers);
 
+void RandonNumbers(int[] numbers)
+{
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+    }
+}
+
+int count = 0;
+
+for (int x = 0; x < numbers.Length; x++)
+{
+    if (numbers[x] % 2 == 0)
+        count++;
+}
+Console.WriteLine($"В этом массиве из {numbers.Length} чисел, {count} четных");
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
 
 
 // Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
